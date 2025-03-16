4b0e72135e49041e605281eb79b3cf62030669b7
polariis <polariis@163.com>
2025-03-16 10:12:20 +0800

    specify the port

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,17 @@
-import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url))
     }
   },
-
   server: {
+    port: 9528,
     hmr: {
       overlay: false
     },
@@ -24,7 +22,7 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api/, "")
       }
     },
-    host: "0.0.0.0",  // ✨✨使项目可通过IP访问
+    host: "0.0.0.0" // ✨✨使项目可通过IP访问
   },
   build: {
     target: "es2020"
